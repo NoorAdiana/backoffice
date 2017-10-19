8d39148741fecda23ed0c4a717aaf6b94ab497b7
Noor Adiana <me@nooradiana.com>
2017-10-19 22:28:34 +0700

    Update seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,6 +13,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(User::class)->create();
+        factory(User::class)->create(['email' => 'admin@backoffice.com', 'name' => 'Administrator']);
     }
 }
